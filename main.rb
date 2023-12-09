694521f0fe8dcfb64ca8336af9e83279032ea41b
matcheydj <matcheydj@users.noreply.github.com>
2023-12-09 16:49:13 -0600

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 # main.rb
 require 'sinatra'
 require './image_manipulation'
+require './svg_tools'
 
 set :port, 3000
 
@@ -42,7 +43,45 @@ post '/generate' do
   # Implement logic for AI-based image generation based on user input
 end
 
+post '/upload' do
+  if params[:image_file]
+    image_path = params[:image_file][:tempfile].path
+    image_manipulation = ImageManipulation.new(image_path)
+
+    # Apply selected effects
+    effects = {}
+    [:affine, :annotate, :arc, :bezier, :circle].each do |effect|
+      if params[:effect_#{effect}]
+        effects[effect] = params[:effect_#{effect}].split(',').map(&:to_f)
+      end
+    end
+
+    image_manipulation.apply_all_effects(effects)
+
+    # Send response
+    send_file "original_#{@image.filename}"
+    send_file "modified_image.png"
+  else
+    redirect '/'
+  end
+end
+
+post '/translate_svg' do
+  if params[:svg_data]
+    dx = params[:dx].to_f
+    dy = params[:dy].to_f
+
+    translated_svg = SvgTools.translate_svg(params[:svg_data], dx, dy)
+
+    # Send response
+    content_type :xml
+    body translated_svg
+  else
+    redirect '/'
+  end
+end
+
 # Views
 erb :index do
-  @effects = [:geometric_patterns, :fractals, :cellular_automata, :pixelate, :gradients, :traditional_colors, :minimalist, :random_colors, :moving_particles]
+  @effects = [:affine, :annotate, :arc, :bezier, :circle, :geometric_patterns, :fractals, :cellular_automata, :pixelate, :gradients, :traditional_colors, :minimalist, :random_colors, :moving_particles]
 end
