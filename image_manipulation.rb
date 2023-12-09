8e15b0ab32a3c3178c13295f72f4a31149c10ce7
matcheydj <matcheydj@users.noreply.github.com>
2023-12-09 16:59:36 -0600

    Update image_manipulation.rb

--- image_manipulation.rb
+++ image_manipulation.rb
@@ -1,354 +1,145 @@
 require 'rmagick'
-require 'benchmark/ips'
 
 class ImageManipulation
   def initialize(image_path)
     @image = Magick::Image.read(image_path).first
-    @effects = [:affine, :annotate, :arc, :bezier, :circle, :geometric_patterns, :fractals, :cellular_automata, :pixelate, :gradients, :traditional_colors, :minimalist, :random_colors, :moving_particles]
+    @effects = [:geometric_patterns, :fractals, :cellular_automata, :pixelate, :gradients, :traditional_colors, :minimalist, :random_colors, :moving_particles]
   end
 
-    # Define individual methods for each effect
-  
-    # Define common logic for applying effects with random colors
-  def apply_effect_with_random_colors(effect)
-    modified_image = get_modified_image(effect)
-    modified_image.modulate(Magick::Pixel.new(rand(256), rand(256), rand(256)))
-    modified_image
-  end
-  
-  def apply_geometric_patterns
-    # Implement logic for applying geometric patterns
-  end
-
-  def apply_fractals
-    # Implement logic for generating a fractal image
-  end
-  
-  def apply_cellular_automata
-    # Apply cellular automata rules for dynamic evolution
-  end
-  
-  def pixelate
-    # Create a retro-inspired pixelated image
-  end
-
-  def gradients
-    # Add gradients to give depth and dimension
-  end
-  
-  def fractals 
-    # Generate a fractal image based on the original
-  end
-  
-  def traditional_colors
-    # Use specific colors based on the image's subject
-  end
-    
-  def minimalist
-    # Convert the image to black, white, and grayscale
-  end
-    
-  def random_colors
-    # Apply a randomized color algorithm
-  end
-      
-  def moving_particles
-    # Add moving particles to the image
-  end
-  
-  # ... define methods for other effects
-
+  # Define individual methods for each effect
   def apply_effect(effect)
     # Implement specific logic for each effect
     case effect
     when :geometric_patterns
-      apply_geometric_patterns
+      apply_geometric_patterns(5, 20, 'black')
     when :fractals
-      apply_fractals
+      apply_fractals('Mandelbrot')
     when :cellular_automata
-      apply_cellular_automata
+      apply_cellular_automata('Conway\'s Game of Life', 100)
     when :pixelate
-      pixelate
+      pixelate(10)
     when :gradients
-      gradients
+      gradients('Horizontal', 'red', 'yellow')
     when :traditional_colors
+      # Use color palette based on image analysis
       traditional_colors
     when :minimalist
       minimalist
     when :random_colors
       random_colors
     when :moving_particles
-      moving_particles
+      moving_particles(50)
     end
 
     # Save the modified image
-    @image.write("modified_#{@image.filename}")
+    @image.write("modified_#{effect}_#{@image.filename}")
   end
 
   def combine_all_effects
     # Combine all modified images into a single one
     combined_image = Magick::ImageList.new
-    Dir.glob("modified_*.png") do |modified_image_path|
-      combined_image << Magick::Image.read(modified_image_path).first
+    @effects.each do |effect|
+      modified_image_path = "modified_#{effect}_#{@image.filename}"
+      combined_image << Magick::Image.read(modified_image_path).first if File.exist?(modified_image_path)
     end
     combined_image.montage("combined_image.png")
   end
-end
 
-  def apply_geometric_patterns
-    # Implement logic for applying geometric patterns
+  private
+
+  def apply_geometric_patterns(lines, distance, color)
     # Create a new image
-  image = Magick::Image.new(500, 500)
+    image = Magick::Image.new(@image.columns, @image.rows)
 
-  # Create a Draw object
-  draw = Magick::Draw.new
-
-  # Set some drawing attributes
-  draw.stroke('black')
-  draw.fill('white')
-
-  # Draw a series of lines to form a geometric pattern
-  (0..500).step(20) do |i|
-  draw.line(0, i, 500, i)
-  draw.line(i, 0, i, 500)
-  end
-
-# Apply the drawing to the image
-draw.draw(image)
-
-# Save the image to a file
-image.write('geometric_pattern.png')
-  end
-
-  def apply_fractals
-    # Implement logic for generating a fractal image
-  end
-  
-  def apply_cellular_automata
-      # Apply cellular automata rules for dynamic evolution
-  end
-  
-  def pixelate
-      # Create a retro-inspired pixelated image
-  end
-
-  def gradients
-      # Add gradients to give depth and dimension
-  end
-  
-  def fractals 
-    # Generate a fractal image based on the original
-  end
-  
-  def traditional_colors
-      # Use specific colors based on the image's subject
-  end
-    
-  def minimalist
-      # Convert the image to black, white, and grayscale
-  end
-    
-  def random_colors
-      # Apply a randomized color algorithm
-  end
-      
-  def moving_particles
-      # Add moving particles to the image
-  end
-  
-    # ... define methods for other effects
-
-  def apply_effect(effect)
-    # Implement specific logic for each effect
-    case effect
-    when :geometric_patterns
-      # Apply geometric patterns to the image
-      @image = Magick::Image.read(geometric_pattern.png).first
-    end
-      def apply_geometric_patterns
     # Create a Draw object
     draw = Magick::Draw.new
 
-    # Set some drawing attributes
-    draw.stroke('black')
-    draw.fill('white')
+    # Set drawing attributes
+    draw.stroke(color)
 
-    # Draw a series of lines to form a geometric pattern
-    (0..@image.columns).step(20) do |i|
+    # Draw lines to form geometric pattern
+    (0..@image.columns).step(distance) do |i|
       draw.line(0, i, @image.columns, i)
       draw.line(i, 0, i, @image.rows)
     end
 
-    # Apply the drawing to the image
+    # Apply drawing to the image
+    draw.draw(image)
+
+    # Modify the original image with the geometric pattern
+    @image = image.composite(@image, Magick::CenterGravity, Magick::OverCompositeOp)
+  end
+
+  def apply_fractals(type)
+    # Use external library for fractal generation
+    fractal_image = FractalGenerator.generate(type, @image.columns, @image.rows)
+
+    # Modify the original image with the generated fractal
+    @image = fractal_image.composite(@image, Magick::CenterGravity, Magick::OverCompositeOp)
+  end
+
+  def apply_cellular_automata(rule, generations)
+    # Use external library for cellular automata simulation
+    cellular_automata = CellularAutomata.simulate(rule, @image.columns, @image.rows, generations)
+
+    # Modify the original image with the simulated cellular automata
+    @image = cellular_automata.composite(@image, Magick::CenterGravity, Magick::OverCompositeOp)
+  end
+
+  def pixelate(size)
+    # Resize the image and scale back to create pixelated effect
+    @image = @image.resize(size, size).resize(@image.columns, @image.rows)
+  end
+
+  def gradients(direction, start_color, end_color)
+    # Create a gradient image and composite it with the original
+    gradient_image = Magick::Image.new(@image.columns, @image.rows) { |im| im.background_color = start_color }
+
+    # Apply the desired gradient
+    case direction
+    when 'Horizontal'
+      gradient_image.gradient_fill('left', end_color)
+    when 'Vertical'
+      gradient_image.gradient_fill('top', end_color)
+    end
+
+    # Composite the gradient over the original image
+    @image = gradient_image.composite(@image, Magick::CenterGravity, Magick::OverCompositeOp)
+  end
+
+  def traditional_colors
+    # Analyze the image and replace colors with dominant ones
+    dominant_colors = image.color_histogram.top_colors(5)
+    dominant_colors.each do |color|
+      @image.quantize(256, Magick::EuclideanDistanceMetric, color)
+    end
+  end
+
+  def minimalist
+    # Convert the image to grayscale and reduce colors
+    @image = @image.quantize(256, Magick::EuclideanDistanceMetric, Magick::Pixel.new(255, 255, 255))
+  end
+
+  def random_colors
+    # Apply random colors to each pixel
+    @image.each_pixel do |pixel, _|
+      pixel.red = rand(256)
+      pixel.green = rand(256)
+      pixel.blue = rand(256)
+    end
+  end
+
+  def moving_particles(count)
+    # Add moving particles of random colors
+    draw = Magick::Draw.new
+    count.times do
+      x = rand(@image.columns)
+      y = rand(@image.rows)
+      radius = rand(5)
+      color = Magick::Pixel.new(rand(256), rand(256), rand(256))
+      draw.circle(x, y, radius, radius)
+      draw.fill(color)
+    end
     draw.draw(@image)
-
-    # Save the modified image
-    @image.write("geometric_#{image_path}")
-  end
-end
-
-# Usage
-image_manipulation = ImageManipulation.new('path_to_your_image.png')
-image_manipulation.apply_geometric_patterns
-
-    ## when :fractals
-      # Generate a fractal image based on the original
-    when :cellular_automata
-      # Apply cellular automata rules for dynamic evolution
-    when :pixelate
-      # Create a retro-inspired pixelated image
-    when :gradients
-      # Add gradients to give depth and dimension
-    when :traditional_colors
-      # Use specific colors based on the image's subject
-    when :minimalist
-      # Convert the image to black, white, and grayscale
-    when :random_colors
-      # Apply a randomized color algorithm
-    when :moving_particles
-      # Add moving particles to the image
-    end
-
-  # Define individual methods for each effect
-  def apply_geometric_patterns
-    # Implement logic for applying geometric patterns
-    # ...
-  end
-
-  def apply_fractals
-    # Implement logic for generating a fractal image
-    # ...
-  end
-
-  # ... define methods for other effects
-
-  def apply_effect(effect)
-    # Use cached modified image if available
-    modified_image = get_modified_image(effect)
-    
-    # Apply effect if not cached
-    unless modified_image
-      modified_image = apply_effect_with_random_colors(effect)
-      @modified_images[effect] = modified_image
-    end
-
-    modified_image
-  end
-
-  def combine_all_effects(output_filename)
-    # Combine all modified images in-memory
-    combined_image = Magick::ImageList.new
-    [:geometric_patterns, :fractals, :pixelate, :gradients, :traditional_colors, :minimalist].each do |effect|
-      modified_image = apply_effect(effect)
-      combined_image << modified_image
-    end
-
-    # Save the combined image
-    combined_image.montage(output_filename)
-
-    # Free memory used by cached images
-    @modified_images.clear
-  end
-
-  def generate_random_combined_image(effects)
-    # Initialize an empty canvas
-    combined_image = Magick::Image.new(@original_image.columns, @original_image.rows) do
-      self.background_color = 'transparent'
-    end
-
-    # Composite effects with transparency
-    effects.each do |effect|
-      modified_image = apply_effect_with_random_colors(effect)
-      combined_image.composite!(modified_image, Magick::CenterGravity, Magick::OverCompositeOp)
-    end
-
-    # Save the combined image with random colors
-    combined_image.write("random_combined.png")
-  end
-
-  # Helper method to retrieve a cached modified image
-  def get_modified_image(effect)
-    @modified_images[effect]
-  end
-end
-
-# Run benchmark for each effect
-Benchmark.ips do |x|
-  image_manipulation = ImageManipulation.new("image.jpg")
-  
-  x.report("Apply Geometric Patterns") { image_manipulation.apply_effect(:geometric_patterns) }
-  x.report("Apply Fractals") { image_manipulation.apply_effect(:fractals) }
-  # ... add benchmarks for other effects
-  x.report("Combine All Effects") { image_manipulation.combine_all_effects("combined.png") }
-end
-
-
-    # Save the modified image
-    @image.write("modified_#{@image.filename}")
-  end
-
-  def combine_all_effects
-    # Combine all modified images into a single one
-    combined_image = Magick::ImageList.new
-    Dir.glob("modified_*.png") do |modified_image_path|
-      combined_image << Magick::Image.read(modified_image_path).first
-    end
-    combined_image.montage("combined_image.png")
-  end
-
-  def generate_random_combined_image(effects)
-    # Initialize an empty canvas
-    combined_image = Magick::Image.new(@image.columns, @image.rows) do
-      self.background_color = 'transparent'
-    end
-
-    # Combine layers with transparency
-    combined_image.composite!(layers[:fractals], Magick::CenterGravity, Magick::OverCompositeOp)
-    combined_image.composite!(layers[:cellular_automata], Magick::CenterGravity, Magick::OverCompositeOp)
-    combined_image.composite!(layers[:moving_particles], Magick::CenterGravity, Magick::OverCompositeOp)
-    
-    # Apply and composite effects with transparency
-    def effects.each do |effect|
-      combined_image.composite!(apply_effect(effect, randomize_colors: true), Magick::CenterGravity, Magick::OverCompositeOp)
-    end
-
-    # Save the combined image with random colors
-    combined_image.write("random_combined.png")
-  end
-
-  def combine_all_effects(output_filename)
-    # Combine all modified images into a single one
-    combined_image = Magick::ImageList.new
-    modified_files = Dir.glob("modified_*.png")
-    combined_image.append(modified_files)
-
-    # Save the combined image with specified filename
-    combined_image.montage(output_filename)
-  end
-end
-
-  def generate_random_combined_image
-  # Initialize an empty canvas
-  combined_image = Magick::Image.new(@image.columns, @image.rows) do
-    self.background_color = 'transparent'
-  end
-
-  # Create a separate layer for each effect
-  layers = {}
-  [:fractals, :cellular_automata, :moving_particles].each do |effect|
-    layers[effect] = apply_effect(effect, randomize_colors: true)
-  end
-
-  # Save the combined image with random colors
-  combined_image.write("random_combined.png")
-end
-
-  def combine_all_effects
-    # Combine all modified images into a single one
-    combined_image = Magick::ImageList.new
-    Dir.glob("modified_*.png") do |modified_image_path|
-      combined_image << Magick::Image.read(modified_image_path).first
-    end
-    combined_image.montage("combined_image.png")
   end
 end
