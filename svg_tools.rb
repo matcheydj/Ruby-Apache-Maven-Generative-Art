ff85a95622a917acbe1f6c30f4cc0107746c8d22
matcheydj <matcheydj@users.noreply.github.com>
2023-12-09 17:03:13 -0600

    Update svg_tools.rb

--- svg_tools.rb
+++ svg_tools.rb
@@ -1,26 +1,55 @@
-# svg_tools.rb
+require "nokogiri"
+
 class SvgTools
-  def self.translate_svg(svg_data, dx, dy)
-    # Parse SVG data
-    doc = Nokogiri::XML(svg_data)
+  def initialize(svg_path)
+    @doc = Nokogiri::XML(File.open(svg_path))
+  end
 
-    # Apply translation
-    doc.xpath('//*[@transform]').each do |element|
-      current_transform = element['transform']
-      element['transform'] = translate_transform(current_transform, dx, dy)
+  def scale(scale_factor)
+    @doc.css("svg").each do |svg|
+      svg["width"] = (svg["width"].to_f * scale_factor).round.to_s
+      svg["height"] = (svg["height"].to_f * scale_factor).round.to_s
     end
+    @doc.css("path, circle, ellipse, rect, line, polygon, polyline").each do |element|
+      element["d"] = transform_path(element["d"], scale_factor)
+    end
+  end
 
-    # Return modified SVG data
-    doc.to_xml
+  def translate(x_shift, y_shift)
+    @doc.css("g").each do |group|
+      group["transform"] = "translate(#{x_shift}, #{y_shift})" + (group["transform"] ? " #{group['transform']}" : "")
+    end
+  end
+
+  def apply_filter(filter_name)
+    # ... Implement logic to apply SVG filters ...
+  end
+
+  def change_color(color)
+    @doc.css("path, circle, ellipse, rect, line, polygon, polyline").each do |element|
+      element["fill"] = color
+      element["stroke"] = color
+    end
+  end
+
+  def save(output_path)
+    File.open(output_path, "w") { |f| f.write(@doc.to_xml) }
   end
 
   private
 
-  def self.translate_transform(transform, dx, dy)
-    match = transform.match(/translate\((.*),(.*)\)/)
-    return transform unless match
+  def transform_path(path_string, scale_factor)
+    path_string.split(/[\s,]+/).map do |segment|
+      case segment[0]
+      when "M", "L", "C", "Q"
+        segment.gsub(/\d+/, &method(:transform_coordinate).curry(scale_factor))
+      else
+        segment
+      end
+    end.join(" ")
+  end
 
-    tx, ty = match.captures.map(&:to_f)
-    "translate(#{tx + dx}, #{ty + dy})"
+  def transform_coordinate(scale_factor, coordinate)
+    (coordinate.to_f * scale_factor).round.to_s
   end
 end
